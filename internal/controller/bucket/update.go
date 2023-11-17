105045b0c539b59906f2318f9d2a850974ab5962
Conor Nolan <cmsnolan@gmail.com.com>
2023-11-17 18:30:58 +0000

    Ensure we exit loop correctly on update + log/comment edits

--- internal/controller/bucket/update.go
+++ internal/controller/bucket/update.go
@@ -135,15 +135,16 @@ func (c *external) updateAll(ctx context.Context, bucket *v1alpha1.Bucket) error
 			continue
 		}
 
-		c.log.Info("Updating bucket", "bucket name", bucket.Name, "backend name", backendName)
-
 		beName := backendName
 		g.Go(func() error {
+			// Set the Bucket status to 'NotReady' until we have successfully performed the update.
 			bucketBackends.setBucketStatus(bucket.Name, beName, v1alpha1.NotReadyStatus)
 
 			for i := 0; i < s3internal.RequestRetries; i++ {
+				c.log.Info("Updating bucket on backend", "bucket_name", bucket.Name, "backend_name", beName)
 				bucketExists, err := s3internal.BucketExists(ctx, cl, bucket.Name)
 				if err != nil {
+					c.log.Info("Error occurred attempting HeadBucket", "err", err.Error(), "bucket_name", bucket.Name, "backend_ name", beName)
 					return err
 				}
 				if !bucketExists {
@@ -152,19 +153,24 @@ func (c *external) updateAll(ctx context.Context, bucket *v1alpha1.Bucket) error
 					return nil
 				}
 
-				bucketBackends.setBucketStatus(bucket.Name, beName, v1alpha1.NotReadyStatus)
-
 				err = c.update(ctx, bucket, beName, bucketBackends)
-				if err == nil {
-					// Check to see if this backend has been marked as 'Unhealthy'. It may be 'Unknown' due to
-					// the healthcheck being disabled. In which case we can only assume the backend is healthy
-					// and mark the bucket as 'Ready' for this backend.
-					if c.backendStore.GetBackendHealthStatus(beName) == apisv1alpha1.HealthStatusUnhealthy {
-						break
-					}
+				if err != nil {
+					c.log.Info("Error occurred attempting to update bucket", "err", err.Error(), "bucket_name", bucket.Name, "backend_ name", beName)
 
-					bucketBackends.setBucketStatus(bucket.Name, beName, v1alpha1.ReadyStatus)
+					continue
 				}
+				// Check if this backend has been marked as 'Unhealthy'. In which case the
+				// Bucket must remain in 'NotReady' state for this backend.
+				if c.backendStore.GetBackendHealthStatus(beName) == apisv1alpha1.HealthStatusUnhealthy {
+
+					return nil
+				}
+				// Bucket has been successfullly updated and the backend is either 'Healthy' or 'Unknown'.
+				// It may be 'Unknown' due to the healthcheck being disabled, in which case we can only assume
+				// the backend is healthy. Either way, set the bucket status as 'Ready' for this backend.
+				bucketBackends.setBucketStatus(bucket.Name, beName, v1alpha1.ReadyStatus)
+
+				return nil
 			}
 
 			return nil

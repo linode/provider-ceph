962627348225fd3dbf6615aa04a4509e8caa50b0
nolancon <cmsnolan@gmail.com>
2024-01-29 16:01:01 +0000

    Add low-level AssumeRole func - unimplemented

---
+++ internal/rgw/assumerole.go
@@ -0,0 +1,30 @@
+package rgw
+
+import (
+	"context"
+
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"github.com/crossplane/crossplane-runtime/pkg/errors"
+	"github.com/linode/provider-ceph/internal/backendstore"
+	"github.com/linode/provider-ceph/internal/otel/traces"
+	"go.opentelemetry.io/otel"
+)
+
+const (
+	errAssumeRole = "failed to assume role"
+)
+
+func AssumeRole(ctx context.Context, stsClient backendstore.STSClient, input *sts.AssumeRoleInput) (*sts.AssumeRoleOutput, error) {
+	ctx, span := otel.Tracer("").Start(ctx, "AssumeRole")
+	defer span.End()
+
+	resp, err := stsClient.AssumeRole(ctx, input)
+	if err != nil {
+		err = errors.Wrap(err, errAssumeRole)
+		traces.SetAndRecordError(span, err)
+
+		return resp, err
+	}
+
+	return resp, nil
+}
